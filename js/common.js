d536892d7cb2c79fcb55a138714a8704ab86a1a9
Tioo <1612956183@qq.com>
2019-08-15 18:25:38 +0800

    tran

--- js/common.js
+++ js/common.js
@@ -1,14 +1,57 @@
+var globalUrl = "http://192.168.8.204:8001";
 
-function sendCode(){//发送验证码
+$(document).ready(function(){
+	console.log($(".sendCode"));
+	$(".sendCode").click(function(){
+		sendCode();
+	})
+})
+function changeState(){//改变按钮状态
+	let count = 60;
+	$('.sendCode').addClass("sendCode-active");
+	const countDown = setInterval(() => {
+		if (count === 0) {
+			$(".sendCode").text('重新发送').removeAttr('disabled');
+			$(".sendCode").removeClass("sendCode-active");
+		clearInterval(countDown);
+		} else {
+			$('.sendCode').attr('disabled', true);
+			$('.sendCode').text(count + '{%trans "秒后可重新获取" %}');
+		}
+		count--;
+	}, 1000);
+}
+function sendCode(){//学生,教师发送手机验证短信
+	changeState();
+	let cellphone = 190130932;
 	$.ajax({
-        type: "GET"/"POST",
-        url: "目标文件.php",
-        data: {
-           所要传到后端的数据
-        },
-        async:true,
-        dataType: "json",
-        sucess:responsRes,
+        type: "GET",
+		url: globalUrl + "/login/a/",
+		data: {
+			cellphone: cellphone
+		},
+		success : function(res) {
+			console.log("success");
+			// let url = globalUrl;
+			// if(res.data.boolteacher == 0){//学生手机号注册
+			// 	url = globalUrl + "/login/sregistersendsms/"
+			// }else if(res.data.boolteacher == 1){//教师手机号注册
+			// 	url = globalUrl + "/login/schangepassendsms/"
+			// }
+			// $.ajax({
+			//     type: "POST",
+			// 	url: url,
+			// 	data: {
+			// 		cellphone: 
+			// 	},
+			// 	success : function(res) {
+			// 		
+			// 　　},
+			//     error:function (err) {
+			//         console.log(err);
+			//     }
+			// });
+	　　},
         error:function (err) {
             console.log(err);
         }
